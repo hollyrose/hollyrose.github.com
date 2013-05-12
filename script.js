8e81b715aae7ea23e5e68b9baacb975c18280746
Holly <holly.lindon@gmail.com>
2013-05-12 22:48:23 +0100

    Added invert to active sidebutton
    Active class no longer applies to the section but only the sideButton

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ $(document).ready(function() {
 	
 	$('.sideButton').on('click', function(event) {
 		//get the data-target
-		var targetId = $(this).data('target')
+		var targetId = $(this).data('target');
 		//.hide() everything with the class .mainbit
-		$('.mainBit').hide()
+		$('.mainBit').hide();
 		//.show() the element with the id that matches data-target
-		$('#' + targetId).show()
+		$('#' + targetId).show();
+		$('.sideButton').removeClass('active');
+		$(this).addClass('active');
 		
 		//ALTERNATIVE
-		$('.mainBit').removeClass('active')
-		$('#' + targetId).addClass('active')
+		//$('.mainBit').removeClass('active')
+		//$('#' + targetId).addClass('active')
 	})
 	
 	
